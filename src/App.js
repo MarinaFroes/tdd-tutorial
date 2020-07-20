4f3627f70ef94958bf37f0b9bf758e7760fad98b
MFAC <ninafroes@gmail.com>
2020-07-20 17:42:59 +0200

    feat: Add PersonList component and import it into App to make the test pass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './App.css'
+import PersonList from './PersonList'
 
 function App() {
   return (
     <div className="App">
-      
+      <PersonList />
     </div>
   );
 }
---
+++ src/PersonList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PersonList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default PersonList
