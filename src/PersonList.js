45784d37376d1073af0a413c053512ed45e5dc99
MFAC <ninafroes@gmail.com>
2020-07-20 22:12:14 +0200

    feat: Add first and last name of the person in the li element

--- src/PersonList.js
+++ src/PersonList.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
-const PersonList = ({ people = [] }) => <ul>{people.map((person, index) => <li key={index}>{person.firstName}</li>)}</ul>
+const PersonList = ({ people = [] }) => <ul>{people.map((person, index) => <li key={index}>{person.firstName} {person.lastName}</li>)}</ul>
 
 export default PersonList
