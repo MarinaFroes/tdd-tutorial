001921060052c9ff2337373172ef84663598c83a
MFAC <ninafroes@gmail.com>
2020-07-20 17:42:39 +0200

    test: Add a test to find a component called PersonList inside App component and make it fail

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import App from './App'
+import PersonList from './PersonList'
 import { shallow } from 'enzyme'
 
 // describe = test suite
@@ -9,5 +10,11 @@ describe('App', () => {
     // appWrapper gets the App component (creates an instance of it) so we can inspect it
     // shallow - only renders the component, not its entire child tree
     const appWrapper = shallow(<App />);
+  });
+
+  // .find() from Jest let us pass a component that we want to find in the current component
+  it('', () => {
+    const appWrapper = shallow(<App />);
+    appWrapper.find(PersonList);
   });
 });
