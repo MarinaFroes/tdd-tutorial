d6b8346fd71de00742039b01d4cf8ca1201ce8c0
MFAC <ninafroes@gmail.com>
2020-07-20 17:29:17 +0200

    test: Create an instance of an adapter for Enzyme to connect with Jest

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,10 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import '@testing-library/jest-dom/extend-expect'
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+// Create an instance of an adapter for React 16 to wrap the component 
+  // This is how Enzyme figures out how to connect itself to Jest
+configure({ adapter: new Adapter()})
