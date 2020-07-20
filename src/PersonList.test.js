431cd335529bc421c0070e49937ba5ac13ab56ae
MFAC <ninafroes@gmail.com>
2020-07-20 18:57:16 +0200

    test: Add a test to check if there's no list items if people prop is an empty array

--- src/PersonList.test.js
+++ src/PersonList.test.js
@@ -3,15 +3,19 @@ import PersonList from './PersonList'
 import { shallow } from 'enzyme'
 
 describe('PersonList', () => {
-  let personListWrapper;
-
-  beforeAll(() => {
-    personListWrapper = shallow(<PersonList />);
-  });
 
   it('renders a ul element', () => {
+    const personListWrapper = shallow(<PersonList />);
     const peopleListUl = personListWrapper.find('ul');
 
     expect(peopleListUl).toHaveLength(1);
+  });
+
+  it('renders no li elements when no people exist', () => {
+    const people = [];
+    const personListWrapper = shallow(<PersonList people={people}/>);
+    const peopleListItems = personListWrapper.find('li');
+
+    expect(peopleListItems).toHaveLength(0);
   });
 })
