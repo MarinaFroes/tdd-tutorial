fce3f2e5e314532eec95ad0b992ccdde09be190a
MFAC <ninafroes@gmail.com>
2020-07-21 16:34:28 +0200

    refactor: Delete all untested code

--- src/AddUserForm.js
+++ src/AddUserForm.js
@@ -1,19 +1,17 @@
 import React, { Component } from 'react'
 
 class AddUserForm extends Component {
-  state = {
-    firstName: "",
-    lastName: ""
-  }
-  
+
   render() {
     return (
       <form>
-        <label htmlFor="first-name">First name</label>
-        <input type="text" value={this.state.firstName} id="first-name" />
-        <label htmlFor="last-name">Last name</label>
-        <input type="text" value={this.state.lastName} id="last-name" />
-        <button>Submit</button>
+        <label>
+          <input type="text" />
+        </label>
+        <label>
+          <input type="text" />
+        </label>
+        <button></button>
       </form>
     )
   }
