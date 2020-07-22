caf8ed7a7d1e7474e69be81bd9676eaac8f62036
MFAC <ninafroes@gmail.com>
2020-07-22 17:12:01 +0200

    test: Check if TextInput has a label element with the htmlFor property of first-name and text of First Name

--- src/TextInput.test.js
+++ src/TextInput.test.js
@@ -5,10 +5,12 @@ import { mount } from 'enzyme'
 describe('TextInput', () => {
   let textInputWrapper;
   let input;
+  let label;
 
   beforeAll(() => {
-    textInputWrapper = mount(<TextInput type="text" name="first_name" label="First Name" />);
+    textInputWrapper = mount(<TextInput type="text" name="first-name" label="First Name" />);
     input = textInputWrapper.find('input');
+    label = textInputWrapper.find('label');
   });
 
   test('renders one text input', () => {
@@ -20,7 +22,19 @@ describe('TextInput', () => {
   });
 
   test('renders one input with name and id of first-name', () => {
-    expect(input.prop('name')).toEqual('first_name');
-    expect(input.prop('id')).toEqual('first_name');
+    expect(input.prop('name')).toEqual('first-name');
+    expect(input.prop('id')).toEqual('first-name');
+  });
+
+  test('renders one label', () => {
+    expect(label).toHaveLength(1);
+  });
+
+  test('renders a label with the text of First Name', () => {
+    expect(label.text()).toEqual('First Name');
+  });
+
+  test('renders a label with htmlFor equal to first-name', () => {
+    expect(label.prop('htmlFor')).toEqual('first-name');
   });
 })
