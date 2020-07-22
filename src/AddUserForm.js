07b9d76bf4479426942ca657a004eed60c92ab4a
MFAC <ninafroes@gmail.com>
2020-07-22 17:16:33 +0200

    feat: Remove inputs and labels and import two TextInput components

--- src/AddUserForm.js
+++ src/AddUserForm.js
@@ -1,17 +1,13 @@
 import React, { Component } from 'react'
+import TextInput from './TextInput'
 
 class AddUserForm extends Component {
 
   render() {
     return (
       <form>
-        <label htmlFor="first-name">First name
-          <input type="text" id="first-name"/>
-        </label>
-        <label htmlFor="last-name">Last name
-          <input type="text" id="last-name"/>
-        </label>
-        <button></button>
+        <TextInput name="first-name" label="First Name" type="text"/>
+        <TextInput name="last-name" label="Last Name" type="text"/>
       </form>
     )
   }
