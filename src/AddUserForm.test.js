8c0143c4207984b068cc64be6da45dbfa0565261
MFAC <ninafroes@gmail.com>
2020-07-22 17:16:16 +0200

    test: Remove tests for input and labels and tests if AddUserForm has two TextInput components

--- src/AddUserForm.test.js
+++ src/AddUserForm.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import AddUserForm from './AddUserForm'
+import TextInput from './TextInput'
 import { shallow } from 'enzyme'
 
 describe('AddUserForm', () => {
@@ -13,49 +14,7 @@ describe('AddUserForm', () => {
     expect(addUserFormWrapper.find('form')).toHaveLength(1);
   });
 
-  test('renders two input fields with type text', () => {
-    expect(addUserFormWrapper.find('input').filterWhere((item) => {
-      return item.prop('type') === 'text';
-    })).toHaveLength(2);
-  });
-
-  test('renders an input with id of first-name', () => {
-    expect(addUserFormWrapper.find('input#first-name')).toHaveLength(1);
-  });
-
-  test('renders an input with id of last-name', () => {
-    expect(addUserFormWrapper.find('input#last-name')).toHaveLength(1);
-  });
-
-  test('renders two labels', () => {
-    expect(addUserFormWrapper.find('label')).toHaveLength(2);
-  });
-
-  test('renders a label with the text First Name', () => {
-    expect(addUserFormWrapper.find('label').filterWhere((item) => {
-      return item.text() === 'First name';
-    })).toHaveLength(1);
-  });
-
-  test('renders a label with the text Last Name', () => {
-    expect(addUserFormWrapper.find('label').filterWhere((item) => {
-      return item.text() === 'Last name';
-    })).toHaveLength(1);
-  });
-
-  test('renders a label with htmlFor property of first-name', () => {
-    expect(addUserFormWrapper.find('label').filterWhere((item) => {
-      return item.prop('htmlFor') === 'first-name';
-    })).toHaveLength(1);
-  });
-
-  test('renders a label with htmlFor property of last-name', () => {
-    expect(addUserFormWrapper.find('label').filterWhere((item) => {
-      return item.prop('htmlFor') === 'last-name';
-    })).toHaveLength(1);
-  });
-
-  test('renders a submit button', () => {
-    expect(addUserFormWrapper.find('button')).toHaveLength(1);
+  test('renders two TextInput', () => {
+    expect(addUserFormWrapper.find(TextInput)).toHaveLength(2);
   });
 });
