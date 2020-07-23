75adb5ea79f97cb74624a83b60ebc450dfe19721
MFAC <ninafroes@gmail.com>
2020-07-23 15:45:47 +0200

    test: Add test to check form submission and remove previous ones

--- src/AddUserForm.test.js
+++ src/AddUserForm.test.js
@@ -1,25 +1,15 @@
 import React from 'react'
 import AddUserForm from './AddUserForm'
-import TextInput from './TextInput'
-import { shallow } from 'enzyme'
-import SubmitButton from './SubmitButton'
+import { mount } from 'enzyme'
 
 describe('AddUserForm', () => {
-  let addUserFormWrapper;
+  
+  test('calls onSubmit prop when form is submitted', () => {
+    const onSubmitFn = jest.fn();
+    const addUserFormWrapper = mount(<AddUserForm onSubmit={onSubmitFn} />);
 
-  beforeAll(() => {
-    addUserFormWrapper = shallow(<AddUserForm />);
-  });
-
-  test('renders one form element', () => {
-    expect(addUserFormWrapper.find('form')).toHaveLength(1);
-  });
-
-  test('renders two TextInput', () => {
-    expect(addUserFormWrapper.find(TextInput)).toHaveLength(2);
-  });
-
-  test('renders one SubmitButton', () => {
-    expect(addUserFormWrapper.find(SubmitButton)).toHaveLength(1);
+    const form = addUserFormWrapper.find('form');
+    form.simulate('submit');
+    expect(onSubmitFn).toHaveBeenCalledTimes(1);
   });
 });
